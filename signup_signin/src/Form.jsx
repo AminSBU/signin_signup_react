b0e49331bd2a9b292e8ed86861a191c049fdc2c4
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-10-25 18:35:09 +0330

    css form for Form

--- signup_signin/src/Form.jsx
+++ signup_signin/src/Form.jsx
@@ -5,7 +5,13 @@ const Form = () => {
     return(
         <>
             <div className="Form-container">
-                
+                <div className="first">
+                    <img src="./src/img/Wavy_Gen-01_Single-07.jpg" className="img"></img>
+                </div>
+
+                <div className="second">
+                    <h1>Hello</h1>                    
+                </div>
             </div>
         </>
     );
