c2a2eccb1225a7203a1758f673bc074102b67d8c
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-10-25 18:10:37 +0330

    set border for form

--- signup_signin/src/Form.jsx
+++ signup_signin/src/Form.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import './Form.css'
 
 const Form = () => {
     return(
         <>
             <div className="Form-container">
-                <h2>Form</h2>
+                
             </div>
         </>
     );
