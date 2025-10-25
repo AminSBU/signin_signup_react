c28f18935059d1a709cbad96a99b84e3070eb5f9
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-10-25 18:07:45 +0330

    main structure of Form.jsx

---
+++ signup_signin/src/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = () => {
+    return(
+        <>
+            <div className="Form-container">
+                <h2>Form</h2>
+            </div>
+        </>
+    );
+}
+
+export default Form;
